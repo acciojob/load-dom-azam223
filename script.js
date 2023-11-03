4b029c4bdd48bba6e992c5758c6e8cb80fe60afe
azam223 <135818819+azam223@users.noreply.github.com>
2023-11-03 16:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    // This function will be executed when the DOM is fully loaded.
+    const body = document.body;
+
+    // Create a text node with the message and append it to the body.
+    const textNode = document.createTextNode("DOM load success");
+    body.appendChild(textNode);
+});
